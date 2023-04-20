8a14cba3d28c0c09f95d911f9209b585eec8fc42
Greensebas <greensebas@hotmail.com>
2023-04-20 18:16:12 -0300

    upd method

--- entregable_2_archivos/main.js
+++ entregable_2_archivos/main.js
@@ -6,9 +6,14 @@ class ProductManager {
     }
 
     async getProducts() {
-        const fileToRead = await fs.promises.readFile(this.path, 'utf-8'); 
-        const products = JSON.parse(fileToRead);
-        return products;
+        try {
+            const fileToRead = await fs.promises.readFile(this.path, 'utf-8'); 
+            const products = JSON.parse(fileToRead);
+            return products;
+        }
+        catch(error) {
+            console.log(error)
+        }
     };
 
     async addProduct( prod ) {
@@ -37,7 +42,7 @@ class ProductManager {
     
             products.push(newProduct);
 
-            await fs.promises.writeFile(this.path, JSON.stringify(products, null, 2))
+            await fs.promises.writeFile(this.path, JSON.stringify(products, null, 2));
             return `Product added successfully with id ${newProduct.id}`;
 
         }
@@ -46,9 +51,59 @@ class ProductManager {
         }
     }
 
-    getProductById(id) {
-        const product = this.products.find(item => item.id === +id);
-        return (product != undefined) ? product : `Product with id: ${id} not found`;
+    async getProductById( id ) {
+        try {
+            const products = await this.getProducts();
+            const product = products.find(item => item.id === +id);
+            return (product != undefined) ? product : `Product with id: '${id}' not found`;
+        }
+        catch(error) {
+            console.log(error)
+        }
+    }
+
+    async deleteProductById( id ) {
+        try {
+            let products = await this.getProducts();
+
+            const product = products.find(item => item.id === +id);
+            if(product == undefined){
+                return `Product with id: '${id}' not found`
+            }
+
+            products = products.filter(item => item.id !== +id);
+
+            await fs.promises.writeFile(this.path, JSON.stringify(products, null, 2));
+            return `Product with id '${id}' was deleted`;
+        }
+        catch(error) {
+            console.log(error);
+        }
+    }
+
+    async updateProduct( id, prod ) {
+        try{
+            let products = await this.getProducts();
+
+            const productIndex = products.findIndex(item => item.id === +id);
+
+            if(productIndex === -1){
+                return `Product with id: '${id}' not found`
+            }
+
+            if(!prod.title || !prod.description || !prod.price || !prod.thumbnail || !prod.code || !prod.stock){
+                return 'empty fields';
+            };
+
+            products[productIndex] = {...prod, id: id};
+
+            await fs.promises.writeFile(this.path, JSON.stringify(products, null, 2));
+            return `Product with id '${id}' was updated`;
+
+        }
+        catch(error){
+            console.log(error);
+        }
     }
 
 };
@@ -63,12 +118,23 @@ let newProduct = {
     stock: 10,
     }
 
+let updProduct = { 
+    title: 'Producto 4', 
+    description: 'Es el producto 4',
+    price: 400,
+    thumbnail: 'Sin imagen', 
+    code: 'cwl526',
+    stock: 40,
+    }
 
 
 const products = new ProductManager('./products.json');
 const asyncFn = async () => {
-    console.log(await products.getProducts());
-    console.log(await products.addProduct(newProduct));
+    // console.log(await products.getProducts());
+    // console.log(await products.addProduct(newProduct));
+    // console.log(await products.getProductById(2));
+    // console.log(await products.deleteProductById(3));
+    console.log(await products.updateProduct( 4, updProduct ));
 }
 
 asyncFn();
