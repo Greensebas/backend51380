9ae36e7d0d0b55c3b6226ed08711863f3caeccd9
Greensebas <greensebas@hotmail.com>
2023-07-23 15:30:54 -0300

    views controllers

--- entregable_07_serverRefactor/src/routes/views.routes.js
+++ entregable_07_serverRefactor/src/routes/views.routes.js
@@ -1,98 +1,17 @@
 import express from 'express';
-import { cartService } from '../controllers/carts.controllers.js';
-import { productService } from '../controllers/products.controllers.js';
-
+import { 
+    getCartByIdRenderController,
+    getLoginGithubRenderController, 
+    getProductsRenderController, 
+    getUserRenderController 
+} from '../controllers/views.controllers.js';
 
 const router = express.Router();
 
-router.get('/', async (req, res) => {
-    try {
-        let user = {};
-        req.session.user?
-            user = {  
-                email: req.session.user.email, 
-                firstName: req.session.user.firstName, 
-                lastName: req.session.user.lastName, 
-                age: req.session.user.age,
-                cartId: req.session.user.cartId,
-                role: req.session.user.role, 
-            } : user = {};
-        res.status(200).render('home', { user });
-    }
-    catch(error) {
-        console.log(error);
-        res.status(500).json({ success: false, result: error.message });
-    }
-});
-
-router.get('/login-github', async (req, res) => {
-    try {
-        res.status(200).render('login-github');
-    }
-    catch(error) {
-        res.status(500).json({ success: false, result: error.message });
-    }
-});
-
-router.get('/products', async (req, res) => {
-    try {
-        const currentUrl = req.originalUrl
-        let { page, limit, sort, category, stock } = req.query;
-        sort = sort ? {price: sort} : null;
-        let query = {}
-        category ? query.category = category : null;
-        stock ? query.stock = { $gt: +stock} : null;
-
-        const user = {  
-            email: req.session.user.email, 
-            firstName: req.session.user.firstName, 
-            lastName: req.session.user.lastName, 
-            age: req.session.user.age,
-            cartId: req.session.user.cartId,
-            role: req.session.user.role, 
-        }
-
-        const queryResult = await productService.getProducts(query, limit, page, sort, currentUrl)
-
-        return res.status(200).render('products', {
-            success: true, 
-            products: queryResult.products,
-            pagination: queryResult.pagination,
-            user: user,
-        });
-    }
-    catch(error) {
-        res.status(500).json({ success: false, result: error.message });
-    }
-});
-
-router.get('/cart/:cid', async (req, res) => {
-    try {
-        const { cid } = req.params;
-        const cart = await cartService.getCartById(cid);
-
-        const simplifiedCart = cart.products.map((item) => {
-            return {
-                title: item.product.title,
-                price: item.product.price,
-                quantity: item.quantity,
-            };
-        });
-
-        return res.status(200).render('cart', { cart: simplifiedCart });
-    }
-    catch(error) {
-        res.status(500).json({ success: false, result: error.message });
-    }
-})
-
-
-
-// router.get('/realTimeProducts', async (req, res) => {
-//     const products = await productService.getProducts()
-//     res.render('realTimeProducts', {products})
-// })
-
+router.get('/', getUserRenderController);
+router.get('/login-github', getLoginGithubRenderController);
+router.get('/products', getProductsRenderController);
+router.get('/cart/:cid', getCartByIdRenderController);
 
 
 export default router;
